7b71fede58c1b212694d0af0b85c445d9ed9350c
DennisdelRioCarbo <104289098+DennisdelRioCarbo@users.noreply.github.com>
2022-07-25 19:52:01 -0700

    added changes

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -4,7 +4,8 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
-// We create the tile layer that will be the background of our map.
+
+
 //  Add a marker to the map for Los Angeles, California.
 L.circleMarker([34.0522, -118.2437], {
     radius: 300,
